44f0ca3cbadf9e2caa6aca7c21189338ac7c80b8
remoteroofers <remoteroofers@gmail.com>
2025-12-28 00:10:05 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,23 +1,43 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDmBLEUz5EWtv1si_UgfKgOiRS8P-wWOnc",
   authDomain: "remoteroofers-51826.firebaseapp.com",
-  projectId: "remoteroofers-51826",
+  projectId: "remoteroofers-51826"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-window.signup = function () {
-  createUserWithEmailAndPassword(auth, email.value, password.value)
-    .then(() => location.href = "order.html")
-    .catch(err => alert(err.message));
-};
+// SIGN UP
+const signupBtn = document.getElementById("signupBtn");
+if (signupBtn) {
+  signupBtn.onclick = () => {
+    createUserWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    )
+    .then(() => window.location.href = "order.html")
+    .catch(e => alert(e.message));
+  };
+}
 
-window.login = function () {
-  signInWithEmailAndPassword(auth, email.value, password.value)
-    .then(() => location.href = "order.html")
-    .catch(err => alert(err.message));
-};
+// LOGIN
+const loginBtn = document.getElementById("loginBtn");
+if (loginBtn) {
+  loginBtn.onclick = () => {
+    signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    )
+    .then(() => window.location.href = "order.html")
+    .catch(e => alert(e.message));
+  };
+}
