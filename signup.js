8249bc5d2fc185794c8931f37abc36376ea272fc
remoteroofers <remoteroofers@gmail.com>
2025-12-28 10:26:33 +0800

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,40 +1,6 @@
-import { auth, db } from "./firebase.js";
-import { createUserWithEmailAndPassword } from 
-  "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { doc, setDoc } from 
-  "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-const form = document.getElementById("signupForm");
-
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const firstName = document.getElementById("firstName").value;
-  const lastName = document.getElementById("lastName").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  try {
-    // Create user
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-
-    const user = userCredential.user;
-
-    // Store user profile in Firestore
-    await setDoc(doc(db, "users", user.uid), {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      createdAt: new Date()
-    });
-
-    alert("Account created successfully!");
-
-  } catch (error) {
-    alert(error.message);
-  }
+await setDoc(doc(db, "users", user.uid), {
+  firstName: firstName,
+  lastName: lastName,
+  email: email,
+  createdAt: new Date()
 });
