46bbbed33cb0596e8dd8c8bfb66a7f74cdb6b969
remoteroofers <remoteroofers@gmail.com>
2026-01-04 04:16:31 +0800

    Update order.js

--- order.js
+++ order.js
@@ -1,13 +1,9 @@
-document.getElementById("orderForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-
-  const data = Object.fromEntries(new FormData(this));
-
-  fetch("YOUR_GOOGLE_SCRIPT_URL", {
-    method: "POST",
-    body: JSON.stringify(data)
-  });
-
-  // Redirect to Stripe
-  window.location.href = "https://buy.stripe.com/YOUR_PAYMENT_LINK";
-});
+function submitOrder() {
+fetch("YOUR_GOOGLE_APPS_SCRIPT_URL", {
+method: "POST",
+body: JSON.stringify({
+package: document.getElementById('package').value,
+date: new Date().toLocaleString()
+})
+}).then(() => alert('Order Submitted'));
+}
