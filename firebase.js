76d3ff18bd943ee76f6327dd4061611f898b7da0
remoteroofers <remoteroofers@gmail.com>
2026-01-04 04:15:02 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDmBLEUz5EWtv1si_UgfKgOiRS8P-wWOnc",
   authDomain: "remoteroofers-51826.firebaseapp.com",
   projectId: "remoteroofers-51826",
-  storageBucket: "remoteroofers-51826.firebasestorage.app",
-  messagingSenderId: "901240361996",
-  appId: "1:901240361996:web:2a209986f74f15618aca29"
 };
 
-const app = initializeApp(firebaseConfig);
 
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-
-
