0a59b07c77a4cb11837ed92f428ea9eec78e364e
remoteroofers <remoteroofers@gmail.com>
2026-01-04 22:28:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ orderForm.onsubmit = async (e) => {
        5. Copy the Web App URL and paste it below in the 'fetch' function.
     */
 
-    const GOOGLE_SCRIPT_URL = 'YOUR_GOOGLE_SCRIPT_WEBHOOK_URL_HERE';
+    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxl6UAUCZvACOzJs9_xY-mNRyJvvHmU118fXV-RDX2pXdlcaKdI-Y1T7K3I_l2jY-SA/exec';
 
     try {
         await fetch(GOOGLE_SCRIPT_URL, {
