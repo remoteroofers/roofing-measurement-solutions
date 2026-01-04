ca5fb831550bc13c352922bf3d9492ad70726182
remoteroofers <remoteroofers@gmail.com>
2026-01-04 10:16:16 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function openOrder() {
-  document.getElementById("orderModal").style.display = "block";
-}
-
-function closeOrder() {
-  document.getElementById("orderModal").style.display = "none";
-}
