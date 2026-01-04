a821895830add229aa949abb5fb3b5f8ff331755
remoteroofers <remoteroofers@gmail.com>
2026-01-04 10:16:00 +0800

    Delete order.js

--- order.js
+++
@@ -1,9 +0,0 @@
-function submitOrder() {
-fetch("YOUR_GOOGLE_APPS_SCRIPT_URL", {
-method: "POST",
-body: JSON.stringify({
-package: document.getElementById('package').value,
-date: new Date().toLocaleString()
-})
-}).then(() => alert('Order Submitted'));
-}
